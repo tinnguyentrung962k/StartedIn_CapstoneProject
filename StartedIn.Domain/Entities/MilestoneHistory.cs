be0765ce226568f78fd2c124924cdccdefa40f83
tinnguyentrung962k <trungtin9620@gmail.com>
2024-10-11 02:00:06 +0700

    fix db relationship for milestone and history

--- StartedIn.Domain/Entities/MilestoneHistory.cs
+++ StartedIn.Domain/Entities/MilestoneHistory.cs
@@ -5,7 +5,9 @@ namespace StartedIn.Domain.Entities;
 
 public class MilestoneHistory : BaseAuditEntity<string>
 {
+    [ForeignKey(nameof(Milestone))]
     public string MilestoneId { get; set; }
     public string Content { get; set; }
     public DateTimeOffset CreatedTime { get; set; } = DateTimeOffset.Now;
+    public Milestone Milestone { get; set; }
 }
---
+++ StartedIn.Domain/Migrations/20241010185612_111020240155.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StartedIn.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class _111020240155 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_MilestoneHistory_MilestoneId",
+                table: "MilestoneHistory",
+                column: "MilestoneId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MilestoneHistory_Milestone_MilestoneId",
+                table: "MilestoneHistory",
+                column: "MilestoneId",
+                principalTable: "Milestone",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MilestoneHistory_Milestone_MilestoneId",
+                table: "MilestoneHistory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MilestoneHistory_MilestoneId",
+                table: "MilestoneHistory");
+        }
+    }
+}
