99ced940b6ad22974df524142dc4eb84f0979e13
Nguyen Trung Tin <trungtin9620@gmail.com>
2024-10-23 12:41:26 +0700

    install signNow package

---
+++ StartedIn.Service/Services/ContractService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using StartedIn.Domain.Entities;
+using StartedIn.Repository.Repositories.Interface;
+using StartedIn.Service.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartedIn.Service.Services
+{
+    public class ContractService : IContractService
+    {
+        private readonly IContractRepository _contractRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<Contract> _logger;
+        public ContractService(IContractRepository contractRepository, 
+            IUnitOfWork unitOfWork, 
+            ILogger<Contract> logger)
+        {
+            _contractRepository = contractRepository;
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+    }
+}
---
+++ StartedIn.Service/Services/Interface/IContractService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartedIn.Service.Services.Interface
+{
+    public interface IContractService
+    {
+    }
+}
