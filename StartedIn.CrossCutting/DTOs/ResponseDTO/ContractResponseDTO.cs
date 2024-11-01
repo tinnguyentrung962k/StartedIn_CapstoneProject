6942b690864288768675903ed25a52d26348c1d0
tinnguyentrung962k <trungtin9620@gmail.com>
2024-11-02 01:35:20 +0700

    git fix response for contract

--- StartedIn.CrossCutting/DTOs/ResponseDTO/ContractResponseDTO.cs
+++ StartedIn.CrossCutting/DTOs/ResponseDTO/ContractResponseDTO.cs
@@ -13,10 +13,13 @@ namespace StartedIn.CrossCutting.DTOs.ResponseDTO
         public string ProjectId { get; set; }
         public string ContractName { get; set; }
         public string ContractType { get; set; }
+        public string ContractStatus { get; set; }
         public string? SignNowDocumentId { get; set; }
         public string ContractPolicy { get; set; }
+        public string ContractIdNumber { get; set; }
         public DateOnly? ValidDate { get; set; }
         public DateOnly? ExpiredDate { get; set; }
+
         public List<UserInContractResponseDTO> UsersInContract { get; set; }
         
     }
