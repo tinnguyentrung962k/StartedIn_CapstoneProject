085e1349d146eddad567bc374f23b8e6b866df4e
NGUYỄN TRUNG TÍN <113057393+tinnguyentrung962k@users.noreply.github.com>
2024-11-17 18:20:53 +0700

    migrate new entity for recruitment and asset management of project

---
+++ StartedIn.Domain/Entities/Asset.cs
@@ -0,0 +1,35 @@
+﻿using StartedIn.CrossCutting.Enum;
+using StartedIn.Domain.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartedIn.Domain.Entities
+{
+    public class Asset : BaseAuditEntity<string>
+    {
+        [ForeignKey(nameof(Project))]
+        public string ProjectId { get; set; }
+
+        [ForeignKey(nameof(Transaction))]
+        public string? TransactionId { get; set; }
+        [MaxLength(100)]
+        public string AssetName { get; set; }
+        
+        [Column(TypeName = "decimal(14,3)")]
+        public decimal? Price { get; set; }
+
+        public DateOnly? PurchaseDate { get; set; }
+        public int? Quantity { get; set; }
+        public AssetStatus? Status { get; set; }
+        
+        [MaxLength(50)]
+        public string? SerialNumber { get; set; }
+        public Transaction? Transaction { get; set; }
+        public Project Project { get; set; }
+    }
+}
