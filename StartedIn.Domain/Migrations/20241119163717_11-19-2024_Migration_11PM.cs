8a414a37310653db3de3254cfb18d255250cc036
walle <buihoanghai2405@gmail.com>
2024-11-19 23:39:43 +0700

    add phase entity

---
+++ StartedIn.Domain/Migrations/20241119163717_11-19-2024_Migration_11PM.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StartedIn.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class _11192024_Migration_11PM : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
