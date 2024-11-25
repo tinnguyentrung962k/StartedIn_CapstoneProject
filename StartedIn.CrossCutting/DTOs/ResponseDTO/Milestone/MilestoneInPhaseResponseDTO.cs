42c59884df6f4339e85afee673332b9df61e3c42
walle <buihoanghai2405@gmail.com>
2024-11-25 23:25:54 +0700

    add identity id

--- StartedIn.CrossCutting/DTOs/ResponseDTO/Milestone/MilestoneInPhaseResponseDTO.cs
+++ StartedIn.CrossCutting/DTOs/ResponseDTO/Milestone/MilestoneInPhaseResponseDTO.cs
@@ -1,10 +1,11 @@
+using StartedIn.CrossCutting.DTOs.BaseDTO;
+
 namespace StartedIn.CrossCutting.DTOs.ResponseDTO.Milestone;
 
-public class MilestoneInPhaseResponseDTO
+public class MilestoneInPhaseResponseDTO : IdentityResponseDTO
 {
     public string Title { get; set; }
     public string? Description { get; set; }
     public DateOnly StartDate { get; set; }
     public DateOnly EndDate { get; set; }
-    public int? Progress { get; set; }
 }
