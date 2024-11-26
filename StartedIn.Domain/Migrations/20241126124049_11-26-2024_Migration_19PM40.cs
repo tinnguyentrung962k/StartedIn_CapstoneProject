933960f45e39b1993b87948e84fd658e05cf1a00
MiteraI <91677179+MiteraI@users.noreply.github.com>
2024-11-26 20:19:37 +0700

    Task comment create

---
+++ StartedIn.Domain/Migrations/20241126124049_11-26-2024_Migration_19PM40.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StartedIn.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class _11262024_Migration_19PM40 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "TaskComment",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskComment_UserId",
+                table: "TaskComment",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskComment_User_UserId",
+                table: "TaskComment",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskComment_User_UserId",
+                table: "TaskComment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskComment_UserId",
+                table: "TaskComment");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "TaskComment");
+        }
+    }
+}
---
+++ StartedIn.Repository/Repositories/TaskCommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using StartedIn.Domain.Context;
+using StartedIn.Domain.Entities;
+using StartedIn.Repository.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartedIn.Repository.Repositories
+{
+    public class TaskCommentRepository : GenericRepository<TaskComment, string>, ITaskCommentRepository
+    {
+        public TaskCommentRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
