f58199a7136549a6e79ef2fba4f8e273bf662906
NGUYỄN TRUNG TÍN <113057393+tinnguyentrung962k@users.noreply.github.com>
2024-12-11 19:25:04 +0700

    request to terminate contract

---
+++ StartedIn.CrossCutting/DTOs/ResponseDTO/TerminationRequest/TerminationRequestResponseDTO.cs
@@ -0,0 +1,22 @@
+﻿using StartedIn.CrossCutting.DTOs.BaseDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartedIn.CrossCutting.DTOs.ResponseDTO.TerminationRequest
+{
+    public class TerminationRequestResponseDTO : IdentityResponseDTO
+    {
+        public string ContractId { get; set; }
+        public string ContractIdNumber { get; set; }
+        public string FromId { get; set; }
+        public string FromName { get; set; }
+        public string ToId { get; set; }
+        public string ToName { get; set; }
+        public string Reason { get; set; }
+        public bool? IsAgreed { get; set; }
+
+    }
+}
