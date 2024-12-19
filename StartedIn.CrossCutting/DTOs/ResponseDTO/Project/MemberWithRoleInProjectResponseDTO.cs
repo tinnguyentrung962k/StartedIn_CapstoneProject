4c380489bbf7c77be97359a3a3da0c40a0c5b114
Minh Pham <tuanminhphamtdn@gmail.com>
2024-12-19 08:52:19 +0700

    add pfp to member response dto

--- StartedIn.CrossCutting/DTOs/ResponseDTO/Project/MemberWithRoleInProjectResponseDTO.cs
+++ StartedIn.CrossCutting/DTOs/ResponseDTO/Project/MemberWithRoleInProjectResponseDTO.cs
@@ -1,11 +1,5 @@
 ﻿using StartedIn.CrossCutting.DTOs.BaseDTO;
 using StartedIn.CrossCutting.Enum;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using StartedIn.CrossCutting.DTOs.BaseDTO;
 
 namespace StartedIn.CrossCutting.DTOs.ResponseDTO.Project
 {
@@ -14,5 +8,6 @@ namespace StartedIn.CrossCutting.DTOs.ResponseDTO.Project
         public string FullName { get; set; }
         public RoleInTeam RoleInTeam { get; set; }
         public string Email { get; set; }
+        public string ProfilePicture { get; set; }
     }
 }
