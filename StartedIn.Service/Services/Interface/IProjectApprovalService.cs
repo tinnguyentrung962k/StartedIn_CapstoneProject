6d97834e92d6a221448b935c7f9695c80a01ffc4
NGUYỄN TRUNG TÍN <113057393+tinnguyentrung962k@users.noreply.github.com>
2024-12-23 19:18:25 +0700

    fix reject for approval

--- StartedIn.Service/Services/Interface/IProjectApprovalService.cs
+++ StartedIn.Service/Services/Interface/IProjectApprovalService.cs
@@ -10,7 +10,7 @@ public interface IProjectApprovalService
     Task<ProjectApproval> CreateProjectApprovalRequest(string userId, string projectId, CreateProjectApprovalDTO createProjectApprovalDto);
     Task<IEnumerable<ProjectApproval>> GetProjectApprovalRequestByProjectId(string userId, string projectId);
     Task ApproveProjectRequest(string approvalId);
-    Task RejectProjectRequest(string approvalId, string rejectReason);
+    Task RejectProjectRequest(string approvalId, CancelReasonApprovalDTO cancelReasonDTO);
     Task<PaginationDTO<ProjectApprovalResponseDTO>> GetAllProjectApprovals(ProjectApprovalFilterDTO filter, int page, int size);
     Task<ProjectApproval> GetProjectApprovalRequestByApprovalId(string userId, string projectId, string approvalId);
     Task<ProjectApproval> GetProjectApprovalRequestByApprovalIdForAdmin(string approvalId);
