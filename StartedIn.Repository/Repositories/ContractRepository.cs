ccc924c89faf0c2519bf0395cbb1d58768c5bbd6
NGUYỄN TRUNG TÍN <113057393+tinnguyentrung962k@users.noreply.github.com>
2025-01-05 15:19:01 +0700

    remove redundant

--- StartedIn.Repository/Repositories/ContractRepository.cs
+++ StartedIn.Repository/Repositories/ContractRepository.cs
@@ -48,35 +48,7 @@ namespace StartedIn.Repository.Repositories
                 .ToListAsync();
             return contract;
         }
-        public async Task<IEnumerable<Contract>> GetContractsByUserIdInAProject(string userId, string projectId, int pageIndex, int pageSize)
-        {
-            pageIndex = pageIndex < 1 ? 0 : pageIndex - 1;
-            pageSize = pageSize < 1 ? 10 : pageSize;
-            var contract = await _appDbContext.Contracts.Where(x => x.ProjectId.Equals(projectId) && x.UserContracts.Any(us => us.UserId.Equals(userId)))
-                .Include(x => x.UserContracts)
-                .ThenInclude(x => x.User)
-                .Include(x => x.Project)
-                .Where(x => x.DeletedTime == null)
-                .Skip(pageIndex * pageSize).Take(pageSize)
-                .ToListAsync();
-            return contract;
-        }
-        public async Task<Contract> GetTheNearestExpiredInternalContractInAProject(string projectId)
-        {
-             var nearestExpiredContract = await _appDbContext.Contracts
-               .Include(x => x.UserContracts)
-               .ThenInclude(x => x.User)
-               .Include(x => x.Project)
-               .Include(x => x.ShareEquities)
-               .Include(x => x.Disbursements)
-               .Include(x => x.DealOffer)
-               .Where(x => x.ProjectId.Equals(projectId) && x.ContractType.Equals(ContractTypeEnum.INTERNAL))
-               .Where(x => x.ExpiredDate < DateOnly.FromDateTime(DateTime.UtcNow))
-               .OrderByDescending(x => x.ExpiredDate)
-               .Where(x => x.DeletedTime == null)
-               .FirstOrDefaultAsync();
-            return nearestExpiredContract;
-        }
+
         public IQueryable<Contract> GetContractListQuery(string userId, string projectId)
         {
             var query = _appDbContext.Contracts
--- StartedIn.Repository/Repositories/Interface/IContractRepository.cs
+++ StartedIn.Repository/Repositories/Interface/IContractRepository.cs
@@ -11,8 +11,6 @@ namespace StartedIn.Repository.Repositories.Interface
     public interface IContractRepository : IGenericRepository<Contract, string>
     {
         Task<Contract> GetContractById(string contractId);
-        Task<IEnumerable<Contract>> GetContractsByUserIdInAProject(string userId, string projectId, int pageIndex, int pageSize);
-        Task<Contract> GetTheNearestExpiredInternalContractInAProject(string projectId);
         Task<List<Contract>> GetContractByProjectId(string projectId);
         IQueryable<Contract> GetContractListQuery(string userId, string projectId);
         Task UpdateUserInContract(UserContract userContract);
